e81d34839d1f8ab6e3c1347dab11c18b2ba8daf0
TheFuip <106837268+TheFuip@users.noreply.github.com>
2022-09-27 00:37:58 -0400

    Best Time to Buy and Sell Stock II
    You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
    
    On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
    
    Find and return the maximum profit you can achieve.

--- bestTimeToBuyAndSellStock.java
+++ bestTimeToBuyAndSellStock.java
@@ -1,53 +1,57 @@
-
 public class bestTimeToBuyAndSellStock {
     public static void main(String[] args) {
         /*
-         * You are given prices where prices[i] is the price of a given stock on the ith
-         * day.
-         * You want to maximize your profit by choosing a single day to buy one stock
-         * and choosing a different day in the future to sell that stock
-         * Return the maximum profit you can acheive from this transaction if you cannot
-         * acheive any profit return 0.
+         * You are given an integer array prices where prices[i] is the price of a given
+         * stock on the ith day.
+         * 
+         * On each day, you may decide to buy and/or sell the stock. You can only hold
+         * at most one share of the stock at any time. However, you can buy it then
+         * immediately sell it on the same day.
+         * 
+         * Find and return the maximum profit you can achieve.
+         * 
+         * Input: prices = [7,1,5,3,6,4]
+         * Output: 7
+         * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit =
+         * 5-1 = 4.
+         * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+         * 3.
+         * Total profit is 4 + 3 = 7.
          */
-        int[] prices = { 2, 4, 1 };
+        int[] prices = {7,1,5,3,6,4};
         System.out.println(maxProfit(prices));
     }
 
-    public static int maxProfit(int[] prices) {
-        //check if array is empty or maybe there is only 1 value inside of prices, if so return 0
-        if (prices == null || prices.length <= 1)
-            return 0;
+    //greedy solution
+    public static int maxProfit(int[] prices){
+        int sell = 0;
 
-        //min will begin at the first index, as such we will start the loop at index 1 instead of 0
-        int min = prices[0]; // min so far
-
-        //will hold the total after the cost from buying + selling
-        int max = 0;
-
-        //start at index 1 end at the final index
-        for (int i = 1; i < prices.length; i++) {
-            //max will be a ternary function Math.max -> it will first contain 0, index[1] value - min (index[0]) from there it will update with current loop data
-            max = Math.max(max, prices[i] - min);
-            //min will hold the first index of 0, and be updated based on if prices[i] is lower than the first index
-            min = Math.min(min, prices[i]);
+        //since we are constantly checking between 2 indexes we set the closing statement of the loop to i < array.length - 1;
+        for(int i = 0; i < prices.length - 1; i++){
+            //here if the i'th value is less than the i + 1'th value, we take sell and add up the profit that can be made by instantly selling in one day
+            //so we start with i + 1 value first since it's greater subtracted by the i'th value since it's less
+            if(prices[i] < prices[i + 1]){
+                sell += prices[i + 1] - prices[i];
+            }
         }
 
-        //just return it 
-        return max;
+        return sell;
     }
 }
 
 /*
- * if(prices==null||prices.length<=1)
- * return 0;
- * 
- * int min=prices[0]; // min so far
- * int result=0;
- * 
- * for(int i=1; i<prices.length; i++){
- * result = Math.max(result, prices[i]-min);
- * min = Math.min(min, prices[i]);
- * }
- * 
- * return result;
+ *  int buy = 0;
+        int sell = 0;
+
+        for(int i = 0; i < prices.length - 1; i++){
+            buy = prices[i];
+
+            if(buy > prices[i + 1]){
+                buy = prices[i + 1];
+            }else{
+                sell += prices[i + 1] - buy;
+            }
+        }
+
+        return sell;
  */
